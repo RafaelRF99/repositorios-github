55c8a4d52fa932b248a9eddbbe05145a296f9d24
Rafael Ferreira <rafaelferreira@FLABEG.COM.BR>
2023-09-04 13:40:39 -0300

    feat: base layout + responsive

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import styles from './Layout.module.scss'
 
 import SideBar from '../../components/SideBar'
 import MenuBar from '../../components/menuBar'
+import Reading from '../Reading'
 
 export default function Layout() {
     return (
@@ -11,7 +12,15 @@ export default function Layout() {
                 <div className={styles.menu}>
                     <MenuBar />
                 </div>
-                <div className={styles.display}></div>
+                <div className={styles.display}>
+                    <div>
+                        <Reading />
+                        <Reading />
+                        <Reading />
+                        <Reading />
+                        <Reading />
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ src/components/Reading/index.tsx
@@ -0,0 +1,15 @@
+import styles from './Reading.module.scss'
+
+export default function Reading() {
+    return (
+        <div className={styles.container}>
+            <h1>Bolsa-Box</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+                nostrum facilis mollitia harum, neque exercitationem vel ex
+                libero! Laboriosam ipsum saepe et perferendis ut quos velit hic
+                quod dicta quibusdam.
+            </p>
+        </div>
+    )
+}
