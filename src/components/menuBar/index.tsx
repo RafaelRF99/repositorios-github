e2a3d2b83bbe8b600985c2bcf127390dd70004cc
Rafael Ferreira <rafaelferreira@FLABEG.COM.BR>
2023-09-04 12:29:32 -0300

    feat: responsive

---
+++ src/components/menuBar/index.tsx
@@ -0,0 +1,9 @@
+import styles from './menuBar.module.scss'
+
+export default function MenuBar() {
+    return (
+        <menu className={styles.container}>
+            <h1>GitHub Repositórios</h1>
+        </menu>
+    )
+}
